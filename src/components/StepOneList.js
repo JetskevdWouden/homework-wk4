516781862bc77f845433e5ed175d0d793f5ed7a7
JetskevdWouden <zegmaarjet@gmail.com>
2019-06-21 23:10:35 +0200

    Start Step1

---
+++ src/components/StepOneList.js
@@ -0,0 +1,16 @@
+import * as React from 'react'
+
+export default class StepOneList extends React.Component {
+    //static propTypes
+    render() {
+        return (
+            //method of displaying recieved props
+        )
+    }
+}
+
+// export default function StepOneList() {
+//     return (
+
+//     )
+// }
